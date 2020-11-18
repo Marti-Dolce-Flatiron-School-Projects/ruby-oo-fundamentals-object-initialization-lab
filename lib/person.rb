4ec8fce4e18b283fa6cd9eb604be055853adede3
Martinique <me@martidolce.com>
2020-11-18 00:14:57 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Author: Martinique Dolce
+# Course: Flatiron School 2020, November 9 - 20201, April 2021
+# Contact: me@martidolce.com | https://modis.martidolce.com
+#
+#
+class Person
+	def initialize(name)
+		@name = name
+	end
+end
